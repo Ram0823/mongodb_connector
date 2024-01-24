2ad6df86de87802d463fced8a2ff38526b30d719
Ram Baldotra <rambaldotra46459244@gmail.com>
2024-01-25 03:11:37 +0530

    Version Updated and code updated

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.6"
 REPO_NAME = "mongodb_connector"
-PKG_NAME= "auto_db_connect"
+PKG_NAME= "unified_database"
 AUTHOR_USER_NAME = "Ram0823"
 AUTHOR_EMAIL = "rambaldotra46459244@gmail.com"
 
@@ -24,7 +24,7 @@ setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    packages=find_packages(where="src")
     )
 
 
